ca41c4b7879f5348f6a11a0372385b9eff848c3b
j0y0nt <4onsocial@gmail.com>
2023-03-23 16:02:31 +0530

    Added formik for validation.

--- vaas-web/vass-fe/src/components/Home/Home.js
+++ vaas-web/vass-fe/src/components/Home/Home.js
@@ -1,8 +1,5 @@
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import {Outlet} from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 
 export default function Home({user, setUser}) {
     
@@ -10,31 +7,6 @@ export default function Home({user, setUser}) {
 	    <Container>
 	    { user && (
 		    <>
-		    <Box
-		sx={{
-		    display: 'flex',
-		    flexWrap: 'wrap',
-		    '& > :not(style)': {
-			m: 1,
-			width: 256,
-			height: 64,
-		    },
-		}}
-		    >
-		    <Paper elevation={3}
-		style={{
-		    backgroundColor: '#2a356b',
-		    color: '#ffffff',
-		    display: 'flex',
-		    justifyContent: 'center',
-		    alignItems: 'center',
-		}}>
-		    <Typography variant="h6" gutterBottom>
-		    Hello {user.name}!
-		</Typography>
-		    </Paper>
-		    </Box>
-		    
 		    <Outlet />
 		    </>
 	    )}
--- vaas-web/vass-fe/src/components/Login/LoginComponent.js
+++ vaas-web/vass-fe/src/components/Login/LoginComponent.js
@@ -8,26 +8,54 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
+import { useFormik } from 'formik';
 
 export default function LoginComponent({user, setUser}){
     const [signup, setSignup] = useState(false);
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
     
-    function handleLogin() {
-	if(username !== '' && username.length > 0 &&
-	   password !== '' && password.length > 0) {
-	    setUser(state => {
-		const user = Object.assign({}, state);
-		user['authorized'] = true;
-		user['name'] = username;
-		return user;
-	    });
-	    navigate("/home", {replace: true});
+    const navigate = useNavigate();
+
+    const validate = values => {
+
+	const errors = {};
+	
+	if (!values.email) {
+	    errors.email = 'Required';
+	} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+	    errors.email = 'Invalid email address';
 	}
+	
+	if (!values.password) {
+	    errors.password = 'Password is required';
+	} else if (values.password.length < 8) {
+	    errors.password = 'Must be 8 characters or more';
+	}
+	return errors;
+    };
+
+    const formik = useFormik({
+	
+	initialValues: {
+	    email: '',
+	    password: ''
+	},
+	validate,
+	onSubmit: values => handleLogin(values),
+    });
+    
+    function handleLogin(userInfo) {
+	
+	setUser(state => {
+	    const user = Object.assign({}, state);
+	    user['authorized'] = true;
+	    user['email'] = userInfo.email;
+	    return user;
+	});
+	navigate("/home", {replace: true});
     }
     
     return (
@@ -41,23 +69,39 @@ export default function LoginComponent({user, setUser}){
             { signup? 'Signup' : 'Login' }
 	</Typography>
 	    
-	    <form>
+	    <form onSubmit={formik.handleSubmit}>
 	    <Grid container spacing={2}>
             <Grid item xs={12}>
 	    <Box style={{width: '100%'}}>
             <FormControl style={{width: '100%'}}>
+	    
 	    <TextField fullWidth  id="outlined-email" label="Email"
-	defaultValue={username}
-	variant="outlined" onChange={e => setUsername(e.target.value)}/>
-	    </FormControl>
+	type="email" required  name="email"
+	onChange={formik.handleChange} value={formik.values.email}
+	variant="outlined" />
+	    
+	{formik.errors.email ?
+	 <FormHelperText error id="my-helper-text">
+	 {formik.errors.email}
+	 </FormHelperText>
+	 : null}
+	</FormControl>
 	    </Box>
             </Grid>
 	    
             <Grid item xs={12}>
 	    <Box style={{width: '100%'}}>
             <FormControl style={{width: '100%'}}>
-	    <TextField id="outlined-pswd" label="Password" defaultValue={password}
-	type="password" variant="outlined" onChange={e => setPassword(e.target.value)}/>
+	    <TextField id="outlined-pswd" label="Password"
+	onChange={formik.handleChange}
+	name="password"
+        value={formik.values.password}
+	type="password" required variant="outlined" />
+	    {formik.errors.password ?
+	 <FormHelperText error id="my-helper-text">
+	 {formik.errors.password}
+	 </FormHelperText>
+	 : null}
 	    </FormControl>
 	    </Box>
             </Grid>
@@ -79,7 +123,8 @@ export default function LoginComponent({user, setUser}){
 	    <Grid item xs={6}>
 	    <Box style={{width: '100%'}}>
 	    <FormControl style={{width: '100%'}}>
-		<Button variant="contained" onClick={e => handleLogin()}>Login</Button>
+		<Button variant="contained" type="submit"
+	    >Login</Button>
 	    </FormControl>
 	    </Box>
             </Grid>
@@ -99,7 +144,8 @@ export default function LoginComponent({user, setUser}){
 	    <Grid item xs={6}>
 	    <Box style={{width: '100%'}}>
 	    <FormControl style={{width: '100%'}}>
-	    <Button variant="contained" onClick={e => setSignup(signup => false)}>Cancel</Button>
+		<Button variant="contained" type="submit"
+	    onClick={e => setSignup(signup => false)}>Cancel</Button>
 	    </FormControl>
 	    </Box>
             </Grid>
