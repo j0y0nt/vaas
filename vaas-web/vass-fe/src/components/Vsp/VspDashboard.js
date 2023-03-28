1c175315583304d3f9c8fbce8e43164921fd2246
j0y0nt <4onsocial@gmail.com>
2023-03-28 13:10:01 +0530

    Add tabs for VSP dashboard.

--- vaas-web/vass-fe/src/components/Vsp/VspDashboard.js
+++ vaas-web/vass-fe/src/components/Vsp/VspDashboard.js
@@ -1,10 +1,30 @@
+import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 export default function VspDashboard() {
+
+    const [value, setValue] = React.useState('vehicles');
+
+    const handleChange = (event, newValue) => {
+	setValue(newValue);
+    };
+    
     return (
 	    <Container>
-	    <Box>Welcome!</Box>
+	    <Box sx={{ width: '100%' }}>
+	    <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="VSP utilities"
+	    >
+            <Tab value="vehicles" label="Vehicles" />
+            <Tab value="bookings" label="Bookings" />
+            <Tab value="earnings" label="Earnings" />
+	    </Tabs>
+	    </Box>
 	    </Container>
     );
 }
